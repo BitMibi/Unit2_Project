35f8b295bc93f1927b912feae8954abd37688821
ETHAN COWAN <2502541@uad.ac.uk>
2025-11-28 09:38:37 +0000

    test

--- Assets/Code/Scripts/CameraControl.cs
+++ Assets/Code/Scripts/CameraControl.cs
@@ -7,38 +7,37 @@ using System.Runtime.CompilerServices;
 public class CameraControl : MonoBehaviour
 {
     //Preset poisitons for the camera to point   
-    public Vector3 position1;       
-    public Vector3 position2;
-    public Vector3 position3;
-    public Vector3 position4;
-    public Vector3 position5;
+    public GameObject cameraModel1;
+    public GameObject cameraModel2;
+    //Ad infinitum for cameras in level
 
 
     private float directionX;
     private float directionY;
 
-    
+    private GameObject previousPosition;
+    private GameObject nextPosition;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = position1;
+        transform.position = cameraModel1.transform.position;
     }
 
     void OnLook()
     {
-        
 
-        switch (transform.position) {           //Attempting a switch case to copy across right and left inputs to switch between the camera positons. idk how i'll figure it out copium
-            case Vector3(position1): transform.position = position2;         break;     
 
-        
-    } 
+        //Make it cycle between camera objects
+
+
+    }
 
 
     // Update is called once per frame
     void Update()
     {
-         
+
     }
 }
