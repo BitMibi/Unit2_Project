f786637e2026608e5b32bcce1f75e3e932b64314
ETHAN COWAN <2502541@uad.ac.uk>
2025-11-28 11:00:36 +0000

    Updated camer control.cs
    Implementing new form of camera control. Currently does not display anything.

--- Assets/Code/Scripts/CameraControl.cs
+++ Assets/Code/Scripts/CameraControl.cs
@@ -2,38 +2,89 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.InputSystem;
 using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 
 public class CameraControl : MonoBehaviour
 {
-    //Preset poisitons for the camera to point   
-    public GameObject cameraModel1;
-    public GameObject cameraModel2;
-    //Ad infinitum for cameras in level
+    //List of cameras in current scene
+    public Camera[] cameraList;
+    
+    
 
 
-    private float directionX;
-    private float directionY;
+    private Camera previousPosition;
+    private Camera currentPosition;
+    private Camera nextPosition;
 
-    private GameObject previousPosition;
-    private GameObject nextPosition;
+    private int previousJump;
+    private int nextJump;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = cameraModel1.transform.position;
+        //Create a position for previous and next to jump to
+        previousJump = cameraList.Length - 1;
+        nextJump = 1;
+
+        //Set the Positions to the objects in cameraList   
+        previousPosition = cameraList[previousJump];
+        currentPosition = cameraList[0];
+        nextPosition = cameraList[nextJump];
+
+
+
+        //Change which cameras are on
+        previousPosition.enabled = false;
+        currentPosition.enabled = true;
+        nextPosition.enabled = false;
+
     }
 
-    void OnLook()
+    void OnPrevious()
     {
+        //Decrease previousJump
+        if (previousJump > 0)
+        {
+            previousJump--;
+        }
+        else { //To avoid going outside array range
+            previousJump = cameraList.Length - 1;
+        }
 
-
-        //Make it cycle between camera objects
-
+        //Switch to previous Camera
+        currentPosition.enabled = false;
+        previousPosition.enabled = true;
+        nextPosition = currentPosition;
+        currentPosition = previousPosition;
+        previousPosition = cameraList[previousJump];
 
     }
 
+    void OnNext()
+    {
+        //Decrease previousJump
+        if (previousJump < cameraList.Length)
+        {
+            nextJump++;
+        }
+        else
+        { //To avoid going outside array range
+            nextJump = 0;
+        }
+
+        //Switch to next camera
+        currentPosition.enabled = false;
+        nextPosition.enabled = true;
+
+        //Change the camera positions
+        previousPosition = currentPosition;
+        currentPosition = nextPosition;
+        nextPosition = cameraList[nextJump];
+        
+    }
 
     // Update is called once per frame
     void Update()
