bc84ee86191f42eaa671cb0e09ddc50d566d953d
BitMibi <ethancowan07@gmail.com>
2025-12-15 14:27:38 +0000

    Buttons and doors finished
    Buttons accurately move up and down, and will open and close the door depending on which are needed to open it. Also added light & heavy variations for heavy & light boxes

--- Assets/Code/Scripts/buttonCheck.cs
+++ Assets/Code/Scripts/buttonCheck.cs
@@ -10,12 +10,15 @@ public class buttonCheck : MonoBehaviour
 
     private Rigidbody rb;
 
-    private bool collisionHappening = false; // Bugfix for if an object leaves the button while something else is still on it it goes up
-    private bool isDown = false; //Used to open door
+    private int collisionsHappening = 0; // Bugfix for if an object leaves the button while something else is still on it it goes up
+    public bool isDown = false; // Public for openDoor
+
+    public float waitTime; 
 
     public GameObject upPos;
     public GameObject downPos; //Up and down positions to move to
-    
+
+    public bool heavyButton; //Bool to differentiate between heavy and light buttons
 
     void Start()
     {
@@ -30,7 +33,7 @@ public class buttonCheck : MonoBehaviour
         for (float i = rb.transform.position.y; i <= upPos.transform.position.y; i += 0.01f)
         {
             rb.MovePosition(new Vector3(rb.transform.position.x, i, rb.transform.position.z));
-            yield return new WaitForSeconds(0.2f);
+            yield return new WaitForSeconds(waitTime);
         }
 
 
@@ -45,34 +48,57 @@ public class buttonCheck : MonoBehaviour
         for (float i = rb.transform.position.y; i >= downPos.transform.position.y; i -= 0.01f)
         {
             rb.MovePosition(new Vector3(rb.transform.position.x, i, rb.transform.position.z));
-            yield return new WaitForSeconds(0.2f);
+            yield return new WaitForSeconds(waitTime);
         }
 
 
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        collisionHappening = true;
-        if (!isDown)
+        string output = $"My name is {name} and I have started a collision with {collision.gameObject.name}.";
+        Debug.Log(output);
+        if (heavyButton && collision.gameObject.name != "LightCube")     //Heavy buttons cannot be pushed down by light cubes.
         {
-            StartCoroutine(Down());
-            isDown = true;
+            collisionsHappening++;
+            if (!isDown && collisionsHappening >= 1)
+            {
+                StartCoroutine(Down());
+                isDown = true;
+            }
+        }
+        else if (!heavyButton)          //Non-heavy buttons can be pushed down by any object
+        {
+            collisionsHappening++;
+            if (!isDown && collisionsHappening >= 1)
+            {
+                StartCoroutine(Down());
+                isDown = true;
+            }
         }
         
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        Debug.Log("Collision exit");
-        if (isDown && !collisionHappening)
+        if (heavyButton && collision.gameObject.name != "LightCube")
         {
-            StartCoroutine(Up());
-            isDown = false;
-            collisionHappening = false;
+            collisionsHappening--;
+            if (isDown && collisionsHappening == 0)
+            {
+                StartCoroutine(Up());
+                isDown = false;
+            }
         }
-        
-         
+        else if (!heavyButton)
+        {
+            collisionsHappening--;
+            if (isDown && collisionsHappening == 0)
+            {
+                StartCoroutine(Up());
+                isDown = false;
+            }
+        }
         
     }
 
--- Assets/Code/Scripts/openDoor.cs
+++ Assets/Code/Scripts/openDoor.cs
@@ -8,12 +8,17 @@ public class openDoor : MonoBehaviour
 
     private Rigidbody rb;
 
-
+    //Booleans to control movement
     private bool opening = false; // To trigger the opening of the door
     private bool closing = false; // To trigger the closing of the door
     private bool isOpen = false; //Checks if door is open or not
+    public GameObject openPos;
+    public GameObject closePos; 
 
-    public bool allButtonsDown = false;
+    //Button Checks
+    public buttonCheck[] buttonsRequired; // Which buttons are required to be down to open the door
+    private bool allButtonsDown = false;    //When all buttons are down, start open script
+    private int buttonCount = 0;    //Increases for amount of buttons active
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -42,8 +47,8 @@ public class openDoor : MonoBehaviour
         isOpen = true;
         opening = false; //Opens Once
 
-        float endPos = rb.transform.position.x + 1.3f;
-        for (float i = rb.transform.position.x; i <= endPos; i += 0.2f)
+        
+        for (float i = rb.transform.position.x; i <= openPos.transform.position.x; i += 0.2f)
         {            
             rb.MovePosition(new Vector3(i, rb.transform.position.y, rb.transform.position.z));
             yield return new WaitForSeconds(0.1f);
@@ -59,8 +64,8 @@ public class openDoor : MonoBehaviour
         closing = false;
 
 
-        float endPos = rb.transform.position.x - 1.3f;
-        for (float i = rb.transform.position.x; i >= endPos; i -= 0.2f)
+        
+        for (float i = rb.transform.position.x; i >= closePos.transform.position.x; i -= 0.2f)
         {
             rb.MovePosition(new Vector3(i, rb.transform.position.y, rb.transform.position.z));
             yield return new WaitForSeconds(0.1f);
@@ -69,10 +74,37 @@ public class openDoor : MonoBehaviour
        
     }
 
-    
+    void buttonsCheck()
+    {
+       
+        for (int i = 0; i < buttonsRequired.Length; i++)
+        {            
+            if (buttonsRequired[i].isDown && buttonCount < buttonsRequired.Length)      //Checks array for amount of buttons down, increases count if true and the count is less than length of array
+            {
+                buttonCount++;                
+            }
+            else if (!buttonsRequired[i].isDown && buttonCount <= buttonsRequired.Length)
+            {
+                buttonCount = 0;                                                          //Resets count if otherwise                 
+            }
+        }
+        if (buttonCount == buttonsRequired.Length)
+        {
+            allButtonsDown = true;
+        }
+        else
+        {
+            allButtonsDown = false;
+        }
+
+    }
+
+
     // Update is called once per frame
     void Update()
     {
+        buttonsCheck();
+        
         if (allButtonsDown)
         {
             opening = true;
