b5dea853d1486830d19465086ec512837e14c55a
BitMibi <ethancowan07@gmail.com>
2025-12-17 06:33:11 +0000

    Final Changes
    tried to bug fix didn't particularly work but the other option had a worse bug so

--- Assets/Code/Scripts/playerControl.cs
+++ Assets/Code/Scripts/playerControl.cs
@@ -101,7 +101,7 @@ public class playerControl : MonoBehaviour
     }
 
     //Checks if player object is in range to pick up objects
-    void OnTriggerStay(Collider liftable)
+    void OnTriggerEnter(Collider liftable)
     {
         inRange = true;
         if (!holding)
